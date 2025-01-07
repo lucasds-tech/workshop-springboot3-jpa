cfe1a7b5fcb39415ba445fefad2f043a8e5d1de5
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-07 17:39:20 -0300

    Project created

---
+++ src/main/java/com/lstech/projectWebServices/ProjectWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lstech.projectWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lstech/projectWebServices/ProjectWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lstech.projectWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
